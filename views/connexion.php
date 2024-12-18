50ab558c3e101d9972d03e1753db13bb6b768aac
istifano <elamiriamir7@gmail.com>
2024-12-18 10:12:01 +0100

    add la foctionnalite du affichage dans la page joueur

--- views/connexion.php
+++ views/connexion.php
@@ -1,12 +1,14 @@
 <?php
 $servername = "db";
-$username = "myuser";
-$password = "mypassword";
-$conn = mysqli_connect($servername, $username, $password);
+$username = "root";
+$password = "rootpassword";
+$database ="gestionnaire";
+$conn = mysqli_connect($servername, $username, $password,$database);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-// echo "Connected successfully";
+//  echo "Connected successfully";
 
 
 ?>
+
