0d7151eb5085c36fc977185af505fc7138408fd1
istifano <elamiriamir7@gmail.com>
2024-12-20 07:13:12 +0100

    j ai ajouterla fonctionaliter  des joueurs

--- views/CrudJoueurs/Ajouter.php
+++ views/CrudJoueurs/Ajouter.php
@@ -17,28 +17,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $dribbling = $_POST['Dribbling'];
     $defending = $_POST['Defending'];
     $physical = $_POST['Physical'];
-
-  
-    $query = "
-    INSERT INTO Players (name, rating, position_player, photo, id_nationality, id_club)
-    VALUES (?, ?, ?, ?, ?, ?)
+    $query_Count = "
+    select  count(*)from  Players 
     ";
-    if ($stmt = mysqli_prepare($conn, $query)) {
-
-        mysqli_stmt_bind_param($stmt, "ssssii", $playerName, $rating, $playerPosition, $photoPlayer, $nationalitiesPlayer, $clubName);
-        if (mysqli_stmt_execute($stmt)) {
-            echo "succ";
-        } else {
-            echo "Error: " . mysqli_error($conn);
-        }
-        mysqli_stmt_close($stmt);
-    } else {
-        echo "Error sur requet";
-    }
-} else {
-    echo "error Form ";
+    $result = mysqli_query($conn,$query_Count);
+    $id = mysqli_fetch_row($result);
+   $idCounter= $id[0]+1;
+//    echo   $idCounter;
 }
+  
+// $query = "INSERT INTO Players (name, rating, position_player, photo, id_nationality, id_club, id_detail_player)
+// VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+// if ($stmt = mysqli_prepare($conn, $query)) {
+//     mysqli_stmt_bind_param($stmt, "sissiiis", );
+
+//     if (mysqli_stmt_execute($stmt)) {
+//         echo "Success";
+//     } else {
+//         echo "Error: " . mysqli_error($conn);
+//     }
+
+//     mysqli_stmt_close($stmt);
+// } else {
+//     echo "Error in query preparation";
+// }
+$query="INSERT INTO Players (name, rating, position_player, photo, id_nationality, id_club, id_detail_player) VALUES ('$playerName', $rating,'$playerPosition', '$photoPlayer','$nationalitiesPlayer', '$clubName', $idCounter)";
+
+if ($stmt = mysqli_query($conn, $query)) {
 
 
-header('Location: ../Joueurs/Jouoeurs.php');
+            echo "Success";
+      } else {
+             echo "Error: " . mysqli_error($conn);
+        }
+// require_once('../CrudJoueurs/Joueurs.php');
+
+// header('Location: ../Joueurs/Jouoeurs.php');
 ?>
